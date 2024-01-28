7e78de662364426f8429c5d9cd091510f9f00f51
Casa\Jose Luis <jlarias690@gmail.com>
2024-01-28 12:31:18 +0100

    Cambios en el JQuery

---
+++ jQuery/Ejercicios Repaso Examen/Ejercicio1/comunes.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".b1").click(function () {
+    $(".subContenedor2").show();
+    $(".subContenedor1").hide();
+    $(".parr").hide();
+    $(".cue").show();
+    $(".acces").hide();
+  });
+});
